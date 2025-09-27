e849365f93bd36d358f7b9f66ecd48d8f941e75e
ekajaya740 <ekajaya740@gmail.com>
2025-09-28 01:13:18 +0800

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_PATH ?? '/invitation/',
   plugins: [react()],
   resolve: {
     alias: {
